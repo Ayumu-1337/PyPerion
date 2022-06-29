387f8adc84a23f0ded0207c2b12fe8319f19de88
Ayumu-1337 <107574880+Ayumu-1337@users.noreply.github.com>
2022-06-29 12:45:28 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,34 +1,32 @@
 from requests import post
+from .hyperion import Hyperion
 
 def obfuscate(
     code: str,
-    camouflate: bool=False,
-    ultrasafemode: bool=False
+    obfcontent: bool=True,
+    addbuiltins: bool=True,
+    camoufle: bool=False,
+    safemode: bool=False,
+    ultrasafemode: bool=False,
+    clean: bool=True,
+    renvars: bool=False,
+    renlibs: bool=False,
+    randlines: bool=False,
+    shell: bool=True
 ):
-    url = "https://api.plague.fun"
-
-    headers = {
-        'authority': 'api.plague.fun',
-        'accept': '*/*',
-        'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-        'content-type': 'text/plain;charset=UTF-8',
-        'origin': 'https://obf.plague.fun',
-        'referer': 'https://obf.plague.fun/',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'sec-gpc': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36',
-    }
-
-    payload = code.removeprefix("\n")
-
-    resp = post(f"{url}?camouflate={str(camouflate).lower()}&ultrasafemode={str(ultrasafemode).lower()}", headers=headers, data=payload)
-
-    if resp.status_code != 200:
-        print("ERROR")
-        return
-
-    obf_code = resp.text
+    
+    obf_code = Hyperion(
+        addbuiltins=addbuiltins,
+        camouflate=camoufle,
+        clean=clean,
+        content=code,
+        obfcontent=obfcontent,
+        randlines=randlines,
+        renlibs=renlibs,
+        renvars=renvars,
+        safemode=safemode,
+        shell=shell,
+        ultrasafemode=ultrasafemode
+    ).content
 
     return obf_code
